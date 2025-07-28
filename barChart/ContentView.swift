776209106d8cc10f84ebc9fbec2fc16785a72269
Tarun Upadhyay <tarunjataura@gmail.com>
2025-07-28 17:14:39 +0530

    Initial Commit

---
+++ barChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  barChart
+//
+//  Created by Tarun Upadhyay on 28/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ barChart/barChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  barChartApp.swift
+//  barChart
+//
+//  Created by Tarun Upadhyay on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct barChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
